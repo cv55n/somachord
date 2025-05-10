5b700b8b341852af2ee0725a6e263ba9dbd79e90
cavassani <lucas.cavassani.q@gmail.com>
2025-05-10 15:48:19 -0300

    adição do idioma `fr`

--- src/modules/build.js
+++ src/modules/build.js
@@ -3,7 +3,7 @@ import * as esbuild from "esbuild";
 export async function buildFront() {
     try {
         await esbuild.build({
-            entryPoints: ['src/front/cobalt.js', 'src/front/cobalt.css'],
+            entryPoints: ['src/front/somachord.js', 'src/front/somachord.css'],
             outdir: `min/`,
             minify: true,
             loader: {".js": "js", ".css": "css"}
